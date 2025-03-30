4177109c4513a723dad46b9b73c85347c7a2a6c6
Alexander Tarasov <alex.tarasov@sentry.io>
2025-03-30 17:34:59 +0200

    add sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,18 @@ import remarkGfm from 'remark-gfm'
 import remarkSmartypants from 'remark-smartypants'
 import rehypeExternalLinks from 'rehype-external-links'
 
+import sentry from '@sentry/astro';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://oioki.me',
-  integrations: [mdx(), svelte()],
+  integrations: [
+    mdx(),
+    svelte(),
+    sentry({
+      dsn: "https://f0378ad435236d1d5ac7140d9cea8d6a@o546955.ingest.us.sentry.io/4509067327635457",
+    })
+  ],
   i18n: {
     defaultLocale: 'en',
     locales: ['de', 'en', 'ru'],
